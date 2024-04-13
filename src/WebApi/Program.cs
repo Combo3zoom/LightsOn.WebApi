796a2349c42ff2b9502835b7a5ed0e4543597cf1
Artur Zaiats <zayats.artur@gmail.com>
2024-04-14 02:12:03 +0300

    Set RoutePrefix to root

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -29,7 +29,7 @@ app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    c.RoutePrefix = "swagger";  
+    c.RoutePrefix = "";  
 });
 
 app.MapEndpoints();
